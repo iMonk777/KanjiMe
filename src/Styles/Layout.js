579cb3b23ac9eef8a1baa9bed40edc96386c0108
iMonk777 <cirstea.andrei@outlook.com>
2020-02-05 00:59:05 +0000

    Created the Character group, with design and logic to ad multiple groups

--- src/Styles/Layout.js
+++ src/Styles/Layout.js
@@ -2,11 +2,24 @@ import {StyleSheet} from 'react-native';
 import {color} from './Color';
 
 export const styles = StyleSheet.create({
-  characterGroup: {
+  characterGroupContainer: {
     backgroundColor: color[0].tiles,
+    marginLeft: 8,
+    marginRight: 8,
+    marginTop: 8,
+    height: 54,
+    alignItems: 'center',
+    flexDirection: 'row',
   },
   icon: {
-    fontSize: 20,
+    fontSize: 32,
     color: color[0].tileIcon,
+    flex: 1,
+  },
+  characterGroupText: {
+    fontFamily: 'Lato',
+    fontSize: 24,
+    width: 173,
+    flex: 8,
   },
 });
