731f1f502838c10b761e4f748107372f1d9ed5bc
iMonk777 <cirstea.andrei@outlook.com>
2020-02-05 21:33:30 +0000

    Moved the flatlist in the Learn tab component

--- src/components/Learning/CharacterGroup.js
+++ src/components/Learning/CharacterGroup.js
@@ -3,41 +3,13 @@ import {Text, View, StyleSheet} from 'react-native';
 import {Icon} from 'native-base';
 import {styles} from '../../Styles/Layout';
 
-// console.warn(color);
 export default class CharacterGroup extends Component {
   render() {
-    // console.warn(color[0].tilesIcon);
-    let groupList = ['Hiragana', 'Katakana', 'Kanji', 'Favorites', 'My List'];
     return (
-      <View>
-        {groupList.map(element => (
-          <View pressHandle={null} style={styles.characterGroupContainer}>
-            <Text style={styles.characterGroupText}> {element} </Text>
-            <Icon name={'right'} type={'AntDesign'} style={styles.icon} />
-          </View>
-        ))}
+      <View pressHandle={null} style={styles.characterGroupContainer}>
+        <Text style={styles.characterGroupText}> {this.props.name} </Text>
+        <Icon name={'right'} type={'AntDesign'} style={styles.icon} />
       </View>
     );
   }
 }
-
-// return (
-//   <View>
-//     <View pressHandle={null} style={styles.characterGroupContainer}>
-//       <Text style={styles.characterGroupText}> Hiragana </Text>
-//       <Icon name={'right'} type={'AntDesign'} style={styles.icon} />
-//     </View>
-//     <View pressHandle={null} style={styles.characterGroupContainer}>
-//       <Text style={styles.characterGroupText}> Katakana </Text>
-//       <Icon name={'right'} type={'AntDesign'} style={styles.icon} />
-//     </View>
-//     <View pressHandle={null} style={styles.characterGroupContainer}>
-//       <Text style={styles.characterGroupText}> Kanji </Text>
-//       <Icon name={'right'} type={'AntDesign'} style={styles.icon} />
-//     </View>
-//     <View pressHandle={null} style={styles.characterGroupContainer}>
-//       <Text style={styles.characterGroupText}> Favorites </Text>
-//       <Icon name={'right'} type={'AntDesign'} style={styles.icon} />
-//     </View>
-//   </View>
-// );
--- src/components/Learning/LearnTab.js
+++ src/components/Learning/LearnTab.js
@@ -1,12 +1,20 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, FlatList} from 'react-native';
 import CharacterGroup from '../Learning/CharacterGroup';
 
 export default class LearnTab extends Component {
   render() {
+    let groupList = [
+      {name: 'Hiragana', id: 1},
+      {name: 'Katakana', id: 2},
+      {name: 'Kanji', id: 3},
+    ];
     return (
       <View>
-        <CharacterGroup />
+        <FlatList
+          data={groupList}
+          renderItem={({item}) => <CharacterGroup name={item.name} />}
+        />
       </View>
     );
   }
